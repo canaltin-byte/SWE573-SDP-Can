95ad9fde842791c939d00c6a629a18f8bcfd0b60
Can Altın <60130204+canaltin-byte@users.noreply.github.com>
2022-12-27 19:02:36 +0300

    making deploy

--- default/settings.py
+++ default/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ax4_-nt)mo6na01=^q+-l#dh)fo-as5xveo)*b*=7-u62il$4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost', 'ec2-18-193-67-141.eu-central-1.compute.amazonaws.com', 'ec2-3-120-192-1.eu-central-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost', 'ec2-18-193-67-141.eu-central-1.compute.amazonaws.com', 'ec2-18-159-208-255.eu-central-1.compute.amazonaws.com']
 
 
 # Application definition
